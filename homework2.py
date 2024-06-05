7209575806ce77fe91c9ebe1fb1567376b21c424
Phxcadet <cadet_phoenix@mail.ru>
2024-06-05 18:51:31 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+done = 12
+time = 1.5
+lesson = 'Python'
+time_for_one = time / done
+print('Курс: ' + lesson + ', всего задач:' + str(done) +
+      ', затрачено часов: ' + str(time) +
+      ', среднее время выполнения ' + str(time_for_one) + ' часа.')
+  

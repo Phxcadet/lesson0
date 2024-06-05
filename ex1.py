1fa214cb5634a5a0c3def5715b154bcef3151f5d
Phxcadet <cadet_phoenix@mail.ru>
2024-06-05 10:05:20 +0700

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,21 @@
+a='hello'
+print(len(a))
+first=5
+second=9
+summa=5 + 9
+diff= 5 - 9
+print(summa)
+print(diff)
+first=8
+second=10
+third=479
+mean=(first + second + third)/3
+print(mean)
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string + ', ' + first_string)
+a=3
+b=4
+c=5
+f=(a*b) + (a*c)
+print((f**3)/2)

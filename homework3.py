7ae7761341b068373c3474cc5b194193800571f3
Phxcadet <cadet_phoenix@mail.ru>
2024-06-06 09:42:23 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Vladimir'
+print('Name: ' + name)
+age = 29
+print('Age: ' + str(age))
+age = age + 10
+print('New age: ' + str(age))
+is_student = True
+print('Is student: ' + str(is_student))

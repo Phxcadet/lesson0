f3220b0ceef7fe16863f447cc9949ec23217ec09
Phxcadet <cadet_phoenix@mail.ru>
2024-07-05 17:06:23 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import *
+from true_math import *
+
+result1 = divide_fake(69, 3)
+result2 = divide_fake(3, 0)
+result3 = divide_true(49, 7)
+result4 = divide_true(15, 0)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
